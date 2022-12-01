4f9db22b89ab433b0003c080a5f341a032b139a9
Martin Palmowski <martin.palmowski@arvato-scs.com>
2022-12-01 10:42:59 +0100

    Part two of the puzzle.

--- day01/calorie-counting.py
+++ day01/calorie-counting.py
@@ -21,4 +21,10 @@ with open(input_file, "r") as input:
         if (max_value is None or num > max_value):
             max_value = num
 
-    print('Maximum calories are:', max_value)
+    print('Maximum calories with one elf are:', max_value)
+
+    elves_backpack.sort(reverse=True)
+    top1 = elves_backpack[0]
+    top2 = elves_backpack[1]
+    top3 = elves_backpack[2]
+    print("Calories with the top 3 are: {0}".format(top1 + top2 + top3))
