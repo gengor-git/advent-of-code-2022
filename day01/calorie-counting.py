0c5d1230390f0916024aeb1144ec0b19f9fd4ec2
Martin Palmowski <martin.palmowski@arvato-scs.com>
2022-12-01 10:37:29 +0100

    Clean up print statements.

--- day01/calorie-counting.py
+++ day01/calorie-counting.py
@@ -10,12 +10,10 @@ with open(input_file, "r") as input:
     for entry in input:
         try:
             fruit = int(entry)
-            # print("entry a number {0}".format(fruit))
             elves_backpack[-1] = elves_backpack[-1] + fruit
         except:
-            # print("entry not a number")
             elves_backpack.append(0)
-    print(len(elves_backpack))
+    print("We've got {0} elves.".format(len(elves_backpack)))
 
     max_value = None
 
@@ -23,4 +21,4 @@ with open(input_file, "r") as input:
         if (max_value is None or num > max_value):
             max_value = num
 
-    print('Maximum value:', max_value)
+    print('Maximum calories are:', max_value)
