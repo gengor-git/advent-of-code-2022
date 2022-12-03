a215476a9134b7bda5aef98751d9eede9e2a9bb0
Martin Palmowski <martin.palmowski@gmail.com>
2022-12-03 11:31:46 +0100

    Part 2.

--- day03/rucksack-reoganization.py
+++ day03/rucksack-reoganization.py
@@ -55,31 +55,39 @@ priorities = {
     'Z': 52
 }
 
-priority_values = 0
-counter = 0
+def get_prio_by_letter(x):
+    return priorities[x]
 
-with open('./day03/input.txt') as rucksacks:
-    for rucksack in rucksacks:
-        counter += 1
-        print("\nRucksack {0}".format(counter))
-        rucksack_size = len(rucksack)-1
-        compartment1 = rucksack[0:int(rucksack_size/2)]
-        compartment2 = rucksack[int(rucksack_size/2):rucksack_size]
-        # print(rucksack)
-        # print(compartment1)
-        # print(compartment2)
-        # print("")
-        if len(compartment1) != len(compartment2):
-            print("ERROR")
-            break;
+def find_double_items():
+    with open('./day03/input.txt') as rucksacks:
+        priority_values = 0
+        counter = 0
+        for rucksack in rucksacks:
+            counter += 1
+            print("\nRucksack {0}".format(counter))
+            rucksack_size = len(rucksack)-1
+            compartment1 = rucksack[0:int(rucksack_size/2)]
+            compartment2 = rucksack[int(rucksack_size/2):rucksack_size]
+            if len(compartment1) != len(compartment2):
+                print("ERROR")
+                break;
 
-        already_found = ""
-        for item in compartment1:
-           if compartment2.count(item) > 0 and item != already_found:
-            already_found = item
-            # print(item)
-            # print(compartment1.count(item))
-            # print(priorities[item])
-            priority_values += priorities[item]
+            already_found = ""
+            for item in compartment1:
+                if compartment2.count(item) > 0 and item != already_found:
+                    already_found = item
+                    priority_values += priorities[item]
 
-    print(priority_values)
+        print(priority_values)
+
+def find_badges():
+    with open("./day03/input.txt") as input:
+        data_raw = [x for x in input.read().splitlines()]
+        data = list(zip(*(iter(data_raw),) * 3))
+        result = sum(get_prio_by_letter((set(group[0]) & set(group[1]) & set(group[2])).pop()) for group in data)
+
+        print(result)
+
+if __name__ == "__main__":
+    # find_double_items()
+    find_badges()
