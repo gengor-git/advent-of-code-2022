55ffa693b33e960d97ad3c7f794fb07b3d376a94
Martin Palmowski <martin.palmowski@gmail.com>
2022-12-05 13:45:26 +0100

    Code cleanup.

--- day05/supply-stacks.py
+++ day05/supply-stacks.py
@@ -1,9 +1,6 @@
 from queue import LifoQueue
 import re
 
-input_file = "./day05/input.txt"
-
-
 def clean(source : str):
     #[\[\]]|[ ]{4}
     return_value = re.sub(r"[\[\]]", "", source)
@@ -15,14 +12,14 @@ def clean(source : str):
     return return_value
 
 
-def part1():
-    data = open(input_file).read().split("\n")
+input_file = "./day05/input.txt"
 
-    stack_strings = data[0:9]
-    moves = data[10:-1]
+data = open(input_file).read().split("\n")
 
-    result = ""
+stack_strings = data[0:9]
+moves = data[10:-1]
 
+def build_stacks() -> list:
     stack1 = LifoQueue()
     stack2 = LifoQueue()
     stack3 = LifoQueue()
@@ -35,8 +32,8 @@ def part1():
 
     all_stacks = [stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9]
 
-    # going through the stacks bottom to top
-    # bottom is higher numbers, top is 0
+# going through the stacks bottom to top
+# bottom is higher numbers, top is 0
     for layer in range(7, -1, -1):
         layer_data = clean(str(stack_strings[layer])).split(" ")
         if len(layer_data) > 9:
@@ -52,6 +49,11 @@ def part1():
     for i in range(0,9):
         print("Stack No. {} has {} elements.".format(i+1, all_stacks[i].qsize()))
 
+    return all_stacks
+
+def part1():
+    result = ""
+    all_stacks = build_stacks()
 
     # let's parse the movement data
     for move in moves:
@@ -73,42 +75,8 @@ def part1():
 
 
 def part2() -> str:
-    data = open(input_file).read().split("\n")
-
-    stack_strings = data[0:9]
-    moves = data[10:-1]
-
     result = ""
-
-    stack1 = LifoQueue()
-    stack2 = LifoQueue()
-    stack3 = LifoQueue()
-    stack4 = LifoQueue()
-    stack5 = LifoQueue()
-    stack6 = LifoQueue()
-    stack7 = LifoQueue()
-    stack8 = LifoQueue()
-    stack9 = LifoQueue()
-
-    all_stacks = [stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9]
-
-    # going through the stacks bottom to top
-    # bottom is higher numbers, top is 0
-    for layer in range(7, -1, -1):
-        layer_data = clean(str(stack_strings[layer])).split(" ")
-        if len(layer_data) > 9:
-            print("ERROR -> Box line to long with {} entries.".format(len(layer_data)))
-            break
-        print(layer_data)
-        for current_stack in range(0,9):
-            if layer_data[current_stack] != "#":
-                print("Stack {} has letter {}".format(current_stack+1,layer_data[current_stack]))
-                all_stacks[current_stack].put(layer_data[current_stack])
-            else:
-                print("No box for stack {}".format(current_stack+1))
-    for i in range(0,9):
-        print("Stack No. {} has {} elements.".format(i+1, all_stacks[i].qsize()))
-
+    all_stacks = build_stacks()
 
     # let's parse the movement data
     for move in moves:
