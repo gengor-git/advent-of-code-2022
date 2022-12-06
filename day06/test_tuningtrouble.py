77f4163c412fb39832388b884694adc478b0be15
Martin Palmowski <martin.palmowski@gmail.com>
2022-12-06 09:26:23 +0100

    Solved part 2.

--- day06/test_tuningtrouble.py
+++ day06/test_tuningtrouble.py
@@ -10,3 +10,12 @@ class TestTuningTrouble(unittest.TestCase):
         self.assertEqual(day6.part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11)
         input = open("./day06/input.txt").read()
         self.assertEqual(day6.part1(input), 1235)
+
+    def testPart2(self):
+        self.assertEqual(day6.part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19)
+        self.assertEqual(day6.part2("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23)
+        self.assertEqual(day6.part2("nppdvjthqldpwncqszvftbrmjlhg"), 23)
+        self.assertEqual(day6.part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29)
+        self.assertEqual(day6.part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26)
+        input = open("./day06/input.txt").read()
+        self.assertEqual(day6.part2(input), 3051)
--- day06/tuningtrouble.py
+++ day06/tuningtrouble.py
@@ -1,4 +1,19 @@
 
+def isLastUnique(datastream) -> bool:
+    val = datastream[-1]
+    compare = datastream[0:-1]
+    print("Looking for '{}' in '{}' from '{}'".format(val, compare, datastream))
+
+    if val not in compare:
+        if len(datastream) >= 2:
+            print("Checking rest of string '{}'.".format(compare))
+            return isLastUnique(compare)
+        else:
+            return True
+    else:
+        False
+
+
 def part1(datastream: str) -> int:
     print("Datastream is '{}'.".format(datastream))
     result = 0
@@ -9,17 +24,23 @@ def part1(datastream: str) -> int:
         print("Checking against '{}'.".format(pre3))
         if datastream[i] not in pre3 and datastream[i-1] not in pre2 and datastream[i-2] != datastream[i-3]:
             return i+1
-
     return result
 
 
 def part2(datastream: str) -> int:
+    print("Datastream is '{}'.".format(datastream))
     result = 0
+    for i in range(13, len(datastream)):
+        print("{} is {}".format(i+1, datastream[i]))
+        if isLastUnique(datastream[i-13:i+1]):
+            return i+1
     return result
 
 
 if __name__ == "__main__":
     # input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
     input = open("day06/input.txt").read()
-    result = part1(input)
-    print("Part 1 result: {}".format(result))
+    # result1 = part1(input)
+    result2 = part2(input)
+    # print("Part 1 result: {}".format(result1))
+    print("Part 2 result: {}".format(result2))  # 23?
