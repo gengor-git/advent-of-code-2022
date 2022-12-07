a9b1f52732773942c4b45005e0be13834050bba3
Martin Palmowski <martin.palmowski@gmail.com>
2022-12-07 20:56:22 +0100

    Added a base test.

--- day07/test_filesystemchecks.py
+++ day07/test_filesystemchecks.py
@@ -3,4 +3,8 @@ import day07.filesystemchecks as day7
 
 
 class TestFileSystemChecks(unittest.TestCase):
-    pass
+    def testMaxSize(self):
+        data = open("day07/input.txt").read().strip().splitlines()
+        root = day7.create_directory_dict(data)
+        sizes = sorted(day7.calculate_sizes(root))
+        self.assertEqual(sizes[-1], 42036703)
